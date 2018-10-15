6b3baf190b403a64139487444cfee02274f8160b
Арляпов Савелий <sava_arl@mail.ru>
2018-10-16 07:22:08 +0800

    fix warnings

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "lib.h"
 
-int main(int argc, char const *argv[]) {
+int main() {
 	std::cout << "build " << version() << std::endl;
 	std::cout << "Hello, World!" << std::endl;
 
